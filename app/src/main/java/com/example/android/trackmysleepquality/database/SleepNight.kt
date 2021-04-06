33a8612b8b7754f60cec5ae9ef3026e2192d5541
gayan <mgdperera@gmail.com>
2021-04-06 21:33:31 +0530

    Add Db feilds andqueries

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -19,7 +19,7 @@ package com.example.android.trackmysleepquality.database
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-
+// add db details
 @Entity(tableName = "daily_sleep_quality_table")
 data class SleepNight  (
 
